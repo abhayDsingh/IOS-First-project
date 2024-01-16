c3cb1899896390d229acd33f6dd440a4f9591375
Abhay Deep Singh <abhaydeepsingh@Abhays-MacBook-Air.local>
2024-01-15 21:43:59 -0500

    first commit

---
+++ First Proj/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  First Proj
+//
+//  Created by Abhay Deep Singh on 1/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: Any) {
+        
+        func changeColor() -> UIColor{
+
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+        
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+}
+
